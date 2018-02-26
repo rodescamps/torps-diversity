cf35713da8dbdefddbfb943f2fce3388a1d32c19
Robin Descamps <robin.descamps@outlook.com>
2018-02-26 02:54:44 +0100

    fix indentation error

--- pathsim_metrics.py
+++ pathsim_metrics.py
@@ -6,10 +6,10 @@ import math
 
 
 def consensusname(log_file):
-    return log_file.split('.')[1]
+  return log_file.split('.')[1]
 
 def build_prob_matrix(log_file):
-  
+
   prob_matrix = {}
   exits = {}
   guards = {}
@@ -25,26 +25,26 @@ def build_prob_matrix(log_file):
       guard_ip = line_fields[2]
       exit_ip = line_fields[4]
       if guard_ip not in guards :
-	    guards[guard_ip] = 0
+        guards[guard_ip] = 0
       if exit_ip not in exits:
-	    exits[exit_ip] = 0
+        exits[exit_ip] = 0
 
       if guard_ip not in prob_matrix:
-	    prob_matrix[guard_ip] = {}
-	    prob_matrix[guard_ip][exit_ip] = 1
+        prob_matrix[guard_ip] = {}
+        prob_matrix[guard_ip][exit_ip] = 1
       elif exit_ip not in prob_matrix[guard_ip]:
-	    prob_matrix[guard_ip][exit_ip] = 1
+        prob_matrix[guard_ip][exit_ip] = 1
       else:
-	    prob_matrix[guard_ip][exit_ip] += 1
+        prob_matrix[guard_ip][exit_ip] += 1
       exits[exit_ip] += 1
       guards[guard_ip]+= 1
 
       counter_line +=1.0
       #if int(counter_line) % 100000 == 0:
-	    #print counter_line
+      #print counter_line
 
 
-  return (counter_line, prob_matrix, guards, exits)  
+  return (counter_line, prob_matrix, guards, exits)
 
 
 def guessing_entropy(log_file):
@@ -59,14 +59,14 @@ def guessing_entropy(log_file):
   guards_marg_probs = {}
   exits_marg_probs = {}
   #for guard, exit_dic in prob_matrix.items():
-   # guards_marg_probs[guard] = sum(exit_dic.values())/counter_line
-    #for guard2, exit_dic2 in prob_matrix.items():
-     # for exit in exit_dic.keys():
-#	if exit in exit_dic2:
-#	  if exit not in exits_marg_probs:
-#	    exits_marg_probs[exit] = prob_matrix[guard2][exit]/counter_line
-#	  else:
-#	    exits_marg_probs[exit] += prob_matrix[guard2][exit]/counter_line
+  # guards_marg_probs[guard] = sum(exit_dic.values())/counter_line
+  #for guard2, exit_dic2 in prob_matrix.items():
+  # for exit in exit_dic.keys():
+  #	if exit in exit_dic2:
+  #	  if exit not in exits_marg_probs:
+  #	    exits_marg_probs[exit] = prob_matrix[guard2][exit]/counter_line
+  #	  else:
+  #	    exits_marg_probs[exit] += prob_matrix[guard2][exit]/counter_line
   i = 2
 
   #pdb.set_trace()
@@ -88,10 +88,10 @@ def guessing_entropy(log_file):
   counter_guards = 0
   counter_exits = 0
   while i < all_nodes:
-    (node_ip, maximum) = get_max_marg_prob(prob_matrix, guards_controlled,\
-	exits_controlled)
+    (node_ip, maximum) = get_max_marg_prob(prob_matrix, guards_controlled, \
+                                           exits_controlled)
     #print "{0}/{1} done".format(i, all_nodes)
-    
+
     prob_list.append(maximum/counter_lines)
     if node_ip in guards and node_ip in exits:
       counter_both += 1
@@ -108,7 +108,7 @@ def guessing_entropy(log_file):
   while i < all_nodes:
     guessing_entropy += (i)*prob_list[i-1]
     i+=1
-   
+
   #print "number of nodes compromised only flagged guards {0}".format(counter_guards)
   #print "number of nodes compromised only flagged exits {0}".format(counter_exits)
   #print "number of nodes compromised flagged both  {0}".format(counter_both)
@@ -127,49 +127,49 @@ def get_max_marg_prob(prob_matrix, guards, exits):
     if guard not in guards:
       for exit in exits:
         if exit in prob_matrix[guard]:
-	  acc += prob_matrix[guard][exit] 
+          acc += prob_matrix[guard][exit]
       if acc > max_guard:
-	max_guard = acc
-	guard_node = guard
+        max_guard = acc
+        guard_node = guard
     for exit, nbr_seen in prob_matrix[guard].items():
       #pdb.set_trace()
       if exit not in exit_seen and exit not in exits:
-	exit_seen[exit] = exit;
-	acc = 0
+        exit_seen[exit] = exit;
+        acc = 0
         for guard2 in guards:
-	  if exit in prob_matrix[guard2]:
-	    acc+= prob_matrix[guard2][exit]
-	if acc > max_exit:
-	  max_exit = acc
-	  exit_node = exit
-	
-  #for guard in prob_matrix:
-    
+          if exit in prob_matrix[guard2]:
+            acc+= prob_matrix[guard2][exit]
+        if acc > max_exit:
+          max_exit = acc
+          exit_node = exit
+
+          #for guard in prob_matrix:
+
     exit = guard
     for guard2 in prob_matrix:
       #verify that the guard-exit is not already taken
-      if exit in prob_matrix[guard2] and guard2 not in guards and exit\
-      not in exits:
+      if exit in prob_matrix[guard2] and guard2 not in guards and exit \
+              not in exits:
         acc_guard = 0
         acc_exit = 0
         for exit2 in exits:
-	  #horizontal line
-	  if exit2 in prob_matrix[guard]:
-	    acc_guard += prob_matrix[guard][exit2]
+          #horizontal line
+          if exit2 in prob_matrix[guard]:
+            acc_guard += prob_matrix[guard][exit2]
         for guard2 in guards:
-	  #vertical line
-	  if exit in prob_matrix[guard2]:
-	    acc_exit+= prob_matrix[guard2][exit]
+          #vertical line
+          if exit in prob_matrix[guard2]:
+            acc_exit+= prob_matrix[guard2][exit]
         acc = acc_guard + acc_exit
         if acc > max_both:
           node_both = guard
-	  max_both = acc
-  
+          max_both = acc
+
   #pdb.set_trace()
   if max(max_both, max(max_exit, max_guard)) == max_both:
     guards[node_both] = node_both
     exits[node_both] = node_both
-    
+
     return (node_both, max_both)
   elif max(max_both, max(max_exit, max_guard)) == max_exit:
     exits[exit_node] = exit_node
@@ -218,7 +218,7 @@ def degree_uniformity_in_circuit(log_file):
       line = line[0:-1]
       line_fields = line.split('\t')
       sample = line_fields[0]
-      
+
       guard_ip = line_fields[2]
       exit_ip = line_fields[4]
       #mean_guard_set_size += int(line_fields[3])
@@ -227,20 +227,20 @@ def degree_uniformity_in_circuit(log_file):
       #else:
       #  mean_exit_set_size += int(line_fields[5])
       if guard_ip not in guard_dic :
-	    guard_dic[guard_ip] = {'seen':1}
-	    count_guard+=1
+        guard_dic[guard_ip] = {'seen':1}
+        count_guard+=1
       else:
-	    guard_dic[guard_ip]['seen']+=1
-  
+        guard_dic[guard_ip]['seen']+=1
+
       if exit_ip not in exit_dic:
-	    exit_dic[exit_ip] = {'seen':1}
+        exit_dic[exit_ip] = {'seen':1}
       else:
-	    exit_dic[exit_ip]['seen'] +=1
+        exit_dic[exit_ip]['seen'] +=1
 
       if guard_ip+exit_ip not in guard_exit_dic:
-	    guard_exit_dic[guard_ip+exit_ip] = {'seen':1}
+        guard_exit_dic[guard_ip+exit_ip] = {'seen':1}
       else:
-	    guard_exit_dic[guard_ip+exit_ip]['seen'] +=1
+        guard_exit_dic[guard_ip+exit_ip]['seen'] +=1
 
       counter_line+=1
   #size_set_guards = len(guard_dic)
@@ -251,19 +251,19 @@ def degree_uniformity_in_circuit(log_file):
   count_exit = counter_line
   count_both = counter_line
   #for guard_ip in guard_dic:
-    #guard_dic[guard_ip]['seen_prob'] =\
-    #float(guard_dic[guard_ip]['seen'])/float(count_guard)
+  #guard_dic[guard_ip]['seen_prob'] =\
+  #float(guard_dic[guard_ip]['seen'])/float(count_guard)
   #for exit_ip in exit_dic:
-    #exit_dic[exit_ip]['seen_prob'] =\
-    #float(exit_dic[exit_ip]['seen'])/float(count_exit)
+  #exit_dic[exit_ip]['seen_prob'] =\
+  #float(exit_dic[exit_ip]['seen'])/float(count_exit)
 
   for guard_ip in guard_dic:
     for exit_ip in exit_dic:
       if guard_ip+exit_ip in guard_exit_dic:
-	    guard_exit_dic[guard_ip+exit_ip]['seen_prob'] =\
-	    float(guard_exit_dic[guard_ip+exit_ip]['seen'])/float(count_both)
+        guard_exit_dic[guard_ip+exit_ip]['seen_prob'] = \
+          float(guard_exit_dic[guard_ip+exit_ip]['seen'])/float(count_both)
       else:
-	    guard_exit_dic[guard_ip+exit_ip] = {'seen_prob':0.0}
+        guard_exit_dic[guard_ip+exit_ip] = {'seen_prob':0.0}
   #guard_list = guard_dic.items()
   #exit_list = exit_dic.items()
   guard_exit_list = guard_exit_dic.items()
@@ -273,7 +273,7 @@ def degree_uniformity_in_circuit(log_file):
   #guard_effectif_set = compute_effective_set_anonymity(guard_list)
   #exit_effectif_set = compute_effective_set_anonymity(exit_list)
   guard_exit_effectif_set = compute_effective_set_anonymity(guard_exit_list)
-  
+
   #print "degree of uniformity ..."
 
 
@@ -291,20 +291,19 @@ def compute_effective_set_anonymity(node_list):
 
 if __name__ == "__main__":
 
-    usage = "" #todo
+  usage = "" #todo
 
-    if len(sys.argv) < 2 :
-        print(usage)
-        sys.exit(1)
-    
-    command = sys.argv[1]
+  if len(sys.argv) < 2 :
+    print(usage)
+    sys.exit(1)
 
-    if command != 'guessing-entropy' and command != "degree-uniformity":
-        print(usage)
-    elif command == 'guessing-entropy':
-        log_file = sys.argv[2]
-        print "{0} {1}".format(consensusname(log_file), guessing_entropy(log_file))
-    elif command == 'degree-uniformity':
-        log_file = sys.argv[2]
-        print "{0} {1}".format(consensusname(log_file), degree_uniformity_in_circuit(log_file))
+  command = sys.argv[1]
 
+  if command != 'guessing-entropy' and command != "degree-uniformity":
+    print(usage)
+  elif command == 'guessing-entropy':
+    log_file = sys.argv[2]
+    print "{0} {1}".format(consensusname(log_file), guessing_entropy(log_file))
+  elif command == 'degree-uniformity':
+    log_file = sys.argv[2]
+    print "{0} {1}".format(consensusname(log_file), degree_uniformity_in_circuit(log_file))
