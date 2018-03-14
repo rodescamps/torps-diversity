992dad509a76d94cf090f63d4fe16d715c65ead6
Robin Descamps <robin.descamps@outlook.com>
2018-03-15 00:44:35 +0100

    IP-AS inference with iptoasn.com

--- as_inference.py
+++ as_inference.py
@@ -1,13 +1,71 @@
 import sys
 import os
-import cPickle as pickle
 from pathsim import *
-import multiprocessing
-import re
-import requests
-import urllib, json
+import csv
+import urllib
+import gzip
 
-
+def ip_in_as(ip, subnets):
+    """
+    Returns True if {ip} is in the range of one of the {subnets},
+    Returns False otherwise.
+    """
+    ipv4 = [int(n) for n in ip.split('.')]
+    for subnets_ranges in subnets:
+        range_start_full, range_end_full = subnets_ranges.split(',')
+        range_start = [int(n) for n in range_start_full.split('.')]
+        range_end = [int(n) for n in range_end_full.split('.')]
+        if ipv4[0] == range_start[0] and ipv4[0] == range_end[0]:
+            if ipv4[1] == range_start[1] and ipv4[1] == range_end[1]:
+                if ipv4[2] == range_start[2] and ipv4[2] == range_end[2]:
+                    if range_start[3] <= ipv4[3] <= range_end[3]:
+                        return True
+                elif range_start[2] <= ipv4[2] <= range_end[2]:
+                    if ipv4[2] == range_start[2]:
+                        if ipv4[3] >= range_start[3]:
+                            return True
+                    elif ipv4[2] == range_end[2]:
+                        if ipv4[3] <= range_end[3]:
+                            return True
+                    else:
+                        return True
+            elif range_start[1] <= ipv4[1] <= range_end[1]:
+                if ipv4[1] == range_start[1]:
+                    if ipv4[2] == range_start[2]:
+                        if ipv4[3] >= range_start[3]:
+                            return True
+                    elif ipv4[2] >= range_start[2]:
+                        return True
+                elif ipv4[1] == range_end[1]:
+                    if ipv4[2] == range_end[2]:
+                        if ipv4[3] <= range_end[3]:
+                            return True
+                    elif ipv4[2] <= range_end[2]:
+                        return True
+                else:
+                    return True
+        elif range_start[0] <= ipv4[0] <= range_end[0]:
+            if ipv4[0] == range_start[0]:
+                if ipv4[1] == range_start[1]:
+                    if ipv4[2] == range_start[2]:
+                        if ipv4[3] >= range_start[3]:
+                            return True
+                    elif ipv4[2] >= range_start[2]:
+                        return True
+                elif ipv4[1] >= range_start[1]:
+                    return True
+            elif ipv4[0] == range_end[0]:
+                if ipv4[1] == range_end[0]:
+                    if ipv4[2] == range_end[2]:
+                        if ipv4[3] <= range_end[3]:
+                            return True
+                    elif ipv4[2] <= range_end[2]:
+                        return True
+                elif ipv4[1] <= range_end[1]:
+                    return True
+            else:
+                return True
+    return False
 
 if __name__ == '__main__':
     usage = 'Usage: as_inference.py [AS number] [logs_in_dir] [top_guards_file] [top_exits_file]\n\
@@ -18,7 +76,7 @@ if __name__ == '__main__':
         print(usage)
         sys.exit(1)
 
-    searched_as_number = int(sys.argv[1])
+    searched_as_number = sys.argv[1]
     in_dir = sys.argv[2]
     guards_file = sys.argv[3]
     exits_file = sys.argv[4]
@@ -29,14 +87,17 @@ if __name__ == '__main__':
                 log_files.append(os.path.join(dirpath,filename))
     log_files.sort(key = lambda x: os.path.basename(x))
 
-    """
-    top_guard_ips = read_compromised_relays_file(guards_in_file)
-    top_exit_ips = read_compromised_relays_file(exits_in_file)
+    # Prepare the AS subnets in DictReader
+    subnets_as_file = urllib.URLopener()
+    subnets_as_file.retrieve("https://iptoasn.com/data/ip2asn-v4.tsv.gz", "ip2asn-v4.tsv.gz")
+    subnets = []
+    with gzip.open('ip2asn-v4.tsv.gz', 'rb') as csvfile:
+        asreader = csv.DictReader(csvfile, ['range_start', 'range_end', 'AS_number', 'country_code', 'AS_description'], dialect='excel-tab')
+        for row in asreader:
+            if row['AS_number'] == searched_as_number:
+                subnets.append(row['range_start']+','+row['range_end'])
 
-    args = (top_guard_ips, top_exit_ips, out_dir, out_name)
-    simulation_analysis(log_files, compromised_top_relays_process_log, \
-                        args)
-    """
+    # Add guards and exits belonging to the searched AS for the guards/exits IP contained in log_files
     as_guards = []
     as_exits = []
     for log_file in log_files:
@@ -51,29 +112,14 @@ if __name__ == '__main__':
                 exit_ip = line_fields[4]
 
                 if guard_ip not in as_guards:
-                    """
-                    autonomous_system = requests.get('http://ipinfo.io/'+guard_ip+'/org?token=18a079694c3e61').text
-                    autonomous_system_number = autonomous_system.split()[0][2:]
-                    """
-                    url = 'https://api.iptoasn.com/v1/as/ip/'+guard_ip+'/'
-                    response = urllib.urlopen(url)
-                    data = json.loads(response.read())
-                    autonomous_system_number = data['as_number']
-                    if autonomous_system_number == searched_as_number:
+                    if ip_in_as(guard_ip, subnets):
                         as_guards.append(guard_ip)
                 if exit_ip not in as_exits:
-                    """
-                    autonomous_system = requests.get('http://ipinfo.io/'+exit_ip+'/org?token=18a079694c3e61').text
-                    autonomous_system_number = autonomous_system.split()[0][2:]
-                    """
-                    url = 'https://api.iptoasn.com/v1/as/ip/'+exit_ip+'/'
-                    response = urllib.urlopen(url)
-                    data = json.loads(response.read())
-                    autonomous_system_number = data['as_number']
-                    if autonomous_system_number == searched_as_number:
+                    if ip_in_as(exit_ip, subnets):
                         as_exits.append(exit_ip)
                 print(as_guards)
                 print(as_exits)
+        lf.close()
 
 
     with open(guards_file, 'w') as gf, open(exits_file, 'w') as ef:
@@ -82,3 +128,6 @@ if __name__ == '__main__':
             gf.write("%s\n" % as_guard)
         for as_exit in as_exits:
             ef.write("%s\n" % as_exit)
+    gf.close()
+    ef.close()
+    csvfile.close()
