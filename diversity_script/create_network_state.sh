bfa029a4284b8146ed8943aa96a4eedb6ba9f34f
Robin Descamps <robin.descamps@outlook.com>
2018-04-04 13:08:10 +0200

    scripts path update

--- diversity_script/create_network_state.sh
+++ diversity_script/create_network_state.sh
@@ -9,4 +9,4 @@ END_MONTH=$4
 IN_DIR=$5
 OUT_DIR=$6
 INIT_DIR=$7
-pypy pathsim.py process --slim --start_year $START_YEAR --start_month $START_MONTH --end_year $END_YEAR --end_month $END_MONTH --in_dir $IN_DIR --out_dir $OUT_DIR --initial_descriptor_dir $INIT_DIR
+nohup pypy ../pathsim.py process --slim --start_year $START_YEAR --start_month $START_MONTH --end_year $END_YEAR --end_month $END_MONTH --in_dir $IN_DIR --out_dir $OUT_DIR --initial_descriptor_dir $INIT_DIR &
--- diversity_script/run_simulations_diversity.sh
+++ diversity_script/run_simulations_diversity.sh
@@ -23,7 +23,7 @@ do
   mkdir -p $OUT_DIR
   while [ $i -lt $PARALLEL_PROCESS ]
   do
-      nohup pypy pathsim.py simulate --nsf_dir $NSF_DIR --num_samples $NUM_SAMPLES --trace_file $TRACEFILE --user_model $USERMODEL --format $OUTPUT --loglevel $LOGLEVEL $PATH_ALG 2> $OUT_DIR/simulate.$EXP_NAME.$NUM_SAMPLES-samples.$i.time 1> $OUT_DIR/simulate.$EXP_NAME.$NUM_SAMPLES-samples.$i.out &
+      nohup pypy ../pathsim.py simulate --nsf_dir $NSF_DIR --num_samples $NUM_SAMPLES --trace_file $TRACEFILE --user_model $USERMODEL --format $OUTPUT --loglevel $LOGLEVEL $PATH_ALG 2> $OUT_DIR/simulate.$EXP_NAME.$NUM_SAMPLES-samples.$i.time 1> $OUT_DIR/simulate.$EXP_NAME.$NUM_SAMPLES-samples.$i.out &
   i=$(($i+1))
   done
 
