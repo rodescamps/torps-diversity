6c5a1fd2ef49c7cf3f3afa9f3da7483def8eb368
Robin Descamps <robin.descamps@outlook.com>
2018-04-16 15:39:38 +0200

    relay adv script correct

--- diversity_script/run_simulations_vanilla_relay_adv.sh
+++ diversity_script/run_simulations_vanilla_relay_adv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Example: sh run_simulations_vanilla_relay_adv.sh ../.. typical tor ../../out/network-states/network-state-2018-01 50000 50000 0 5 5 1 270
+# Example: sh run_simulations_vanilla_relay_adv.sh ../.. typical tor ../../out/network-states/ 50000 50000 0 5 5 1 270
 
 BASE_DIR=$1
 USERMODEL=$2
 PATH_ALG=$3
-NSF_DIR=$4
+NSF_ROOT_DIR=$4
 
 ADV_GUARD_CONS_BW=$5
 ADV_EXIT_CONS_BW=$6
@@ -23,12 +23,18 @@ LOGLEVEL="INFO"
 PARALLEL_PROCESS=`nproc --all`
 # PARALLEL_PROCESS=8
 
+DIRS=`ls -l $NSF_ROOT_DIR | egrep '^d' | awk '{print $9}'`
 i=0
-EXP_NAME=$USERMODEL.${NSF_DIR}
-OUT_DIR=$BASE_DIR/out/simulate/$PATH_ALG/$EXP_NAME/$OUTPUT/out
-mkdir -p $OUT_DIR
-while [ $i -lt $PARALLEL_PROCESS ]
+for DIR in $DIRS
 do
-  nohup python ../pathsim.py simulate --nsf_dir $NSF_DIR --num_samples $NUM_SAMPLES --trace_file $TRACEFILE --user_model $USERMODEL --format $OUTPUT --adv_guard_cons_bw $ADV_GUARD_CONS_BW --adv_exit_cons_bw $ADV_EXIT_CONS_BW --adv_time $ADV_TIME --num_adv_guards $NUM_ADV_GUARDS --num_adv_exits $NUM_ADV_EXITS --num_guards $NUM_GUARDS --guard_expiration $GUARD_EXPIRATION --loglevel $LOGLEVEL $PATH_ALG 2> $OUT_DIR/simulate.$EXP_NAME.$NUM_SAMPLES-samples.$i.time 1> $OUT_DIR/simulate.$EXP_NAME.$NUM_SAMPLES-samples.$i.out &
-i=$(($i+1))
+  EXP_NAME=$USERMODEL.${DIR}
+  NSF_DIR=$NSF_ROOT_DIR/${DIR}
+  OUT_DIR=$BASE_DIR/out/simulate/$PATH_ALG/$EXP_NAME/$OUTPUT/out
+  mkdir -p $OUT_DIR
+  while [ $i -lt $PARALLEL_PROCESS ]
+  do
+     nohup python ../pathsim.py simulate --nsf_dir $NSF_DIR --num_samples $NUM_SAMPLES --trace_file $TRACEFILE --user_model $USERMODEL --format $OUTPUT --adv_guard_cons_bw $ADV_GUARD_CONS_BW --adv_exit_cons_bw $ADV_EXIT_CONS_BW --adv_time $ADV_TIME --num_adv_guards $NUM_ADV_GUARDS --num_adv_exits $NUM_ADV_EXITS --num_guards $NUM_GUARDS --guard_expiration $GUARD_EXPIRATION --loglevel $LOGLEVEL $PATH_ALG 2> $OUT_DIR/simulate.$EXP_NAME.$NUM_SAMPLES-samples.$i.error 1> $OUT_DIR/simulate.$EXP_NAME.$NUM_SAMPLES-samples.$i.out &
+  i=$(($i+1))
+  done
+
 done
