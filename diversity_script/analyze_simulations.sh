ac83ce23f5b01366a7327eb103256c652556bf59
Robin Descamps <robin.descamps@outlook.com>
2018-04-16 14:36:33 +0200

    modif scripts

--- diversity_script/analyze_simulations.sh
+++ diversity_script/analyze_simulations.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Example: sh analyze_simulations.sh simulation-top ../../out/simulate/tor/typical.network-state-2017-01/ ../../top_guards ../../top_exits ../../out/analysis custom_name
+# Example: sh analyze_simulations.sh simulation-top ../../out/simulate/tor/typical.network-state-2018-01/out/ ../../out/simulate/tor/typical.network-state-2018-01/as/16276_guards ../../out/simulate/tor/typical.network-state-2018-01/as/16276_exits ../../out/analysis 16276
 
 PLOT_TYPE=$1
 IN_DIR=$2
