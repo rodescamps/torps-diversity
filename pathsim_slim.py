be8a0aa3de3be9b548955b01c954416188100238
Robin Descamps <robin.descamps@outlook.com>
2018-06-03 22:54:25 +0200

    print debug

--- pathsim_slim.py
+++ pathsim_slim.py
@@ -2034,6 +2034,7 @@ def compute_probabilities(network_states, water_filling, denasa, tier1_as_advers
                                     provider_as = link["asn"]
                                     if link["asn"] not in list_as_encountered:
                                         list_as_encountered.append(link["asn"])
+                                    print("recursion: {}".format(link["asn"]))
                                     add_prefixes(str(provider_as))
                             if not provider_found:
                                 list_providers.append(searched_as_number)
