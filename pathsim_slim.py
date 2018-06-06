eaf22d0a3657ed41c6931a6420b29bfd46e8cc74
Robin Descamps <robin.descamps@outlook.com>
2018-06-06 02:30:04 +0200

    auto detection of top adversaries

--- pathsim_slim.py
+++ pathsim_slim.py
@@ -2226,8 +2226,11 @@ def compute_probabilities(network_states, water_filling, denasa, tier1_as_advers
                         customer_cone_subnets_adversaries[customer_cone_as].append(line)
                 ccf.close()
 
-    # DeNASA g-select
+    # Analysis of tier-1 ASes compromises
+    top_as_probability = 0.0
+
     if denasa:
+        # DeNASA g-select
         for address in guards:
             for as_customer_cone, subnets in customer_cone_subnets.items():
                 if as_customer_cone in g_select:
@@ -2235,10 +2238,6 @@ def compute_probabilities(network_states, water_filling, denasa, tier1_as_advers
                         guards.remove(address)
                         break
 
-    # Analysis of tier-1 ASes compromises
-    top_as_probability = 0.0
-
-    if denasa:
         for as_customer_cone, subnets in customer_cone_subnets_adversaries.items():
             if as_customer_cone not in e_select:
                 print(as_customer_cone)
@@ -3037,7 +3036,7 @@ commands', dest='pathalg_subparser')
         if args.pathalg_subparser == 'tor-denasa':
             denasa = True
 
-        tier1_as_adversaries = ['3356', '1299', '174', '2914', '3257']
+        tier1_as_adversaries = []#['3356', '1299', '174', '2914', '3257']
         # top_as_paths_compromised, top_as_first_compromise, as_variance not used when guessing entropy computed
         (guards_probabilities, exits_probabilities,
          guards_number, exits_number,
@@ -3151,7 +3150,7 @@ commands', dest='pathalg_subparser')
         #network_states_it1, network_states_it2 = itertools.tee(network_states, 1)
         #network_states_list = list(network_states)
 
-        tier1_as_adversaries = ['5511', '3491', '1299', '701', '2828'] #['3356', '1299', '174', '2914', '3257']
+        tier1_as_adversaries = []#['5511', '3491', '1299', '701', '2828']
 
         (guards_probabilities, exits_probabilities,
          guards_number, exits_number,
@@ -3167,13 +3166,13 @@ commands', dest='pathalg_subparser')
         if args.top_as is not None:
             top_as_number.append(int(args.top_as))
         else:
-            top_as_number = ['16276'] # [16276, 12876, 24940, 14061]
+            top_as_number = [] # [16276, 12876]
         # Top country are DE (Germany), FR (France), NL (Netherlands), US (United States) by default
         top_country_code = []
         if args.top_country is not None:
             top_country_code.append(args.top_country)
         else:
-            top_country_code = [] # ['DE', 'FR', 'NL', 'US']
+            top_country_code = [] # ['DE', 'FR']
 
         (as_paths_compromised, as_first_compromise, as_variance, top_as_adversary) = as_compromise_path(guards_probabilities,
                                                                          exits_probabilities,
